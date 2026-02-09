a1cb6a278cec2b81993c89ad700c5bd6274c1abd
Sagor <161376518+0xsagor@users.noreply.github.com>
2026-02-09 13:34:49 +0600

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+    RPC_URL: "wss://eth-mainnet.g.alchemy.com/v2/YOUR_KEY",
+    // Uniswap V2 Router
+    UNISWAP_ROUTER: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    // SushiSwap Router
+    SUSHISWAP_ROUTER: "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
+    // Token Addresses (WETH and USDC)
+    WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    TRADE_AMOUNT: "1.0" // Amount of WETH to simulate
+};
